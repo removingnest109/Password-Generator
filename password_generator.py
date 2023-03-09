7c1b2101591a03c387872ce597f55263c74611e5
removingnest109 <127269871+removingnest109@users.noreply.github.com>
2023-03-09 13:14:04 -0600

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,38 @@
+import random
+import os
+import string
+import configparser
+
+config_file = 'config.ini'
+if not os.path.isfile(config_file):
+    # generate config file
+    config = configparser.ConfigParser()
+    config['keys'] = {'password_length': 16}
+    with open(config_file, 'w') as f:
+        config.write(f)
+
+config = configparser.ConfigParser()
+config.read(config_file)
+password_length = int(config.get('keys', 'password_length'))
+
+per_type = int(password_length / 4)
+leftover = password_length - per_type * 3
+uppercase_letters = string.ascii_uppercase
+lowercase_letters = string.ascii_lowercase
+digits = string.digits
+symbols = '()!@#$%&*/?'
+
+password = ''.join(
+    [random.choice(uppercase_letters) for _ in range(per_type)] + 
+    [random.choice(digits) for _ in range(per_type)] +
+    [random.choice(symbols) for _ in range(per_type)] +
+    [random.choice(lowercase_letters) for _ in range(leftover)])
+
+output_path = os.getcwd()
+with open(os.path.join(output_path, 'output.txt'), 'w') as file:
+    password_list = list(password)
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+    file.write(password)
+
+os.startfile(os.path.join(output_path, 'output.txt'))
