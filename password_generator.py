6ef6b5bd8ec9af684dfb7747c12b7fed467defdb
Ethan <127269871+removingnest109@users.noreply.github.com>
2024-01-30 12:42:53 -0600

    Update password_generator.py
    removed config file and made variable names more clear

--- password_generator.py
+++ password_generator.py
@@ -1,23 +1,8 @@
 import os
-import string
-import configparser
-config_file = 'config.ini'
-if not os.path.isfile(config_file):
-    # generate config file
-    config = configparser.ConfigParser()
-    config['keys'] = {'password_length': 16}
-    with open(config_file, 'w') as f:
-        config.write(f)
-config = configparser.ConfigParser()
-config.read(config_file)
-password_length = int(config.get('keys', 'password_length'))
-allowed_chars = string.ascii_letters + string.digits + '()!@#$%&*?'
+password_length = 16
 password = ''
 while len(password) < password_length:
-    char = chr(os.urandom(1)[0])
-    if char in allowed_chars:
-        password += char
-output_path = os.getcwd()
-with open(os.path.join(output_path, 'output.txt'), 'w') as file:
-    file.write(password)
-os.startfile(os.path.join(output_path, 'output.txt'))
+    new_character = chr(os.urandom(1)[0])
+    if new_character in '()!@#$%&*?<>/\^-_=+qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890':
+        password += new_character
+print(password)
