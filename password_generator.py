4a46d73bcc4989016d4ed7aba0859e245162314f
Ethan Saenger <ethan@saenger5.com>
2024-05-13 09:18:44 -0500

    rebase

--- password_generator.py
+++
@@ -1,8 +0,0 @@
-import os
-password_length = 16
-password = ''
-while len(password) < password_length:
-    new_character = chr(os.urandom(1)[0])
-    if new_character in '()!@#$%&*?<>/\^-_=+qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890':
-        password += new_character
-print(password)
