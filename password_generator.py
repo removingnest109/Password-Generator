b3257f056854aa16c6697fa3bd41cda26ed81a89
Ethan Saenger <ethan@saenger5.com>
2024-05-13 09:40:25 -0500

    added cli arg for length

--- password_generator.py
+++ password_generator.py
@@ -1,11 +1,28 @@
 import os
+import sys
 
-password_length = 16
-password = ''
+def generate_password(password_length):
+    password = ''
+    while len(password) < password_length:
+        new_character = chr(ord(os.urandom(1)))
+        if new_character in '()!@#$%&*?<>/^-_=+qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890':
+            password += new_character
+    return password
 
-while len(password) < password_length:
-    new_character = chr(os.urandom(1)[0])
-    if new_character in '()!@#$%&*?<>/^-_=+qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890':
-        password += new_character
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <password_length>")
+        sys.exit(1)
 
-print(password)
+    try:
+        password_length = int(sys.argv[1])
+    except ValueError:
+        print("Error: Password length must be an integer.")
+        sys.exit(1)
+
+    if password_length <= 0:
+        print("Error: Password length must be greater than 0.")
+        sys.exit(1)
+
+    password = generate_password(password_length)
+    print(password)
